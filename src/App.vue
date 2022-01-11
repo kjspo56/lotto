51d40159e7bf6b503019312095ff1368a08bb8d1
kjspo56 <kjspo56@gmail.com>
2022-01-11 18:45:29 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    'Header': Header,
+    'Content': Content,
+    'Footer': Footer,
+  }
+}
+</script>
